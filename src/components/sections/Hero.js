a0bd2b5d3925350b913bcbed1ec49ed9722aefb8
abhishek <abhishekwagle2015@gmail.com>
2021-06-29 15:46:48 +0545

    modification in conatct us

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,7 +61,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-            Empower Train Learn <span className="text-color-primary">Grow</span>
+            Empower Train Learn <span className="text-color-high">Grow</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom text-color-high" data-reveal-delay="400">
@@ -71,9 +71,6 @@ const Hero = ({
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
                     Get enrolled now
-                    </Button>
-                 <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
                     </Button>
                 </ButtonGroup>
               </div>
